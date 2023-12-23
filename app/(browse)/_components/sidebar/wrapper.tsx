8e1ca859412724a65753f32c96cbd686cb5e0868
Adle24 <askaradilet41@gmail.com>
2023-12-23 15:09:59 +0600

    Added recommended list

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -5,29 +5,37 @@ import { cn } from "@/lib/utils";
 import { useState, useEffect } from "react";
 import { ToggleSkeleton } from "./toggle";
 import { RecommendedSkeleton } from "./recommended";
+import { FollowingSkeleton } from "./following";
 
 interface WrapperProps {
-    children: React.ReactNode;
-};
+  children: React.ReactNode;
+}
 
-export const Wrapper = ({children} : WrapperProps) => {
-    const [isClient, setIsClient] = useState(false);
-    const { collapsed } = useSidebar((state) => state);
+export const Wrapper = ({ children }: WrapperProps) => {
+  const [isClient, setIsClient] = useState(false);
+  const { collapsed } = useSidebar((state) => state);
 
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-    if (!isClient) return (
-        <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
-            <ToggleSkeleton />
-            <RecommendedSkeleton />
-        </aside>
-    );
-
+  if (!isClient)
     return (
-        <aside className={cn("fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50", collapsed && "w-[70px]")}>
-            {children}
-        </aside>
+      <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+        <ToggleSkeleton />
+        <FollowingSkeleton />
+        <RecommendedSkeleton />
+      </aside>
     );
+
+  return (
+    <aside
+      className={cn(
+        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+        collapsed && "w-[70px]"
+      )}
+    >
+      {children}
+    </aside>
+  );
 };
