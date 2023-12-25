ae2a1787dd39b1401417759e24cfd562deeafd8d
Adle24 <askaradilet41@gmail.com>
2023-12-25 22:59:23 +0600

    Added stream model and chat settings page

---
+++ app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Switch } from "@/components/ui/switch";
+import { toast } from "sonner";
+import { updateStream } from "@/actions/stream";
+import { useTransition } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+
+type FieldTypes = "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly";
+
+interface ToggleCardProps {
+  label: string;
+  value: boolean;
+  field: FieldTypes;
+}
+
+export const ToggleCard = ({
+  label,
+  value = false,
+  field,
+}: ToggleCardProps) => {
+  const [isPending, startTransition] = useTransition();
+  const OnChange = () => {
+    startTransition(() => {
+      updateStream({ [field]: !value })
+        .then(() => toast.success("Chat settings updated!"))
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+  return (
+    <div className="rounded-xl bg-muted p-6">
+      <div className="flex items-center justify-between">
+        <p className="font-semibold shrink-0">{label}</p>
+        <div className="space-y-2">
+          <Switch
+            onCheckedChange={OnChange}
+            disabled={isPending}
+            checked={value}
+          >
+            {value ? "On" : "Off"}
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const ToggleCardSkeleton = () => {
+  return <Skeleton className="rounded-xl p-10 w-full" />;
+};
